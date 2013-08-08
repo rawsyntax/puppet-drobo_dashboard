8f5c6e725f2b21b904064d33aa08f4329a2bd326
Eric Himmelreich <eric@rawsyntax.com>
2013-08-07 21:45:48 -0400

    Run the installer after setting up the dmg

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,5 +7,10 @@ class drobo_dashboard {
   package { 'DroboDashboard':
     provider => 'appdmg',
     source   => 'http://www.drobo.com/start/download/dashboard/Drobo-Dashboard-installer-2.5.2.dmg',
+    ensure => installed
+  }
+
+  exec {'run the installer':
+    command => '/usr/bin/open /Applications/Install.app'
   }
 }
